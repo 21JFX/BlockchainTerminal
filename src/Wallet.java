972cc28b9a95a9cfe1b8b0b139947f5cc8600ec9
Kuba <108862908+21JFX@users.noreply.github.com>
2022-09-16 12:29:28 +0200

    Update Wallet.java

--- src/Wallet.java
+++ src/Wallet.java
@@ -19,17 +19,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Wallet {
-    public PrivateKey privateKey;
+    public PrivateKey privateKey; //protected 
     public PublicKey publicKey;
-    public HashMap<String, TransactionOutput> UTXOs = new HashMap<>();
-    public static ArrayList<Wallet> wallets = new ArrayList<>();
+    public Map<String, TransactionOutput> UTXOs = new HashMap<>(); //final
+    public static List<Wallet> wallets = new ArrayList<>(); //final
     Wallet(){
         generateKeyPair();
     }
 
     public void generateKeyPair() {
         try {
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG"); //public static final STRING
             ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("B-571");
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
             // Initialize the key generator and generate a KeyPair
@@ -39,12 +39,12 @@ public class Wallet {
             privateKey = keyPair.getPrivate();
             publicKey = keyPair.getPublic();
         }catch (Exception e){
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); //logging
         }
     }
 
     //as the name says
-    public float getBalance(){
+    public float getBalance(){ //big decimal 
         float total = 0;
         for (Map.Entry<String, TransactionOutput> item : PlataoPlomo.UTXOs.entrySet()){
             TransactionOutput UTXO = item.getValue();
@@ -57,11 +57,11 @@ public class Wallet {
     }
     public Transaction sendFunds(PublicKey _recipient, float value){
         if(getBalance()<value){
-            System.out.println("Not enough funds to make a transaction");
+            System.out.println("Not enough funds to make a transaction"); //logger
             return null;
         }
-        ArrayList<TransactionInput> inputs = new ArrayList<>();
-        float total = 0;
+        List<TransactionInput> inputs = new ArrayList<>(); //set size map.entrySet(setSize())
+        float total = 0; //add after filtering
         for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
             TransactionOutput UTXO = item.getValue();
             total += UTXO.value;
@@ -78,7 +78,7 @@ public class Wallet {
     }
 
 
-    public static void printWalletDetails(){
+    public static void printWalletDetails(){ //add output stream as a param
         int _wallet;
         ArrayList<String> arr = new ArrayList<>();
         for(int i=1; i<wallets.size()-1;i++){
@@ -86,7 +86,7 @@ public class Wallet {
         }
         Scanner wallet = new Scanner(System.in);
         System.out.println("Which wallet you want to see?"+
-                " Available wallets:\n "+ arr +"\n Choose index from [1-"+ wallets.size()+"]"
+                " Available wallets:\n "+ arr +"\n Choose index from [1-"+ wallets.size()+"]" //never,log instead
                 );
         _wallet = wallet.nextInt();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
